41b1a07682b71c9e4ebf6700ae3e8d0bbb402803
임강호 <55732952+Icln@users.noreply.github.com>
2024-04-17 19:05:01 +0900

    Title: 코드트리 투어, Time: 5012ms, Memory: 49MB, Status: Time Limit Exceed - CodeTree

--- 240417/코드트리 투어/codetree-tour.py
+++ 240417/코드트리 투어/codetree-tour.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
-import sys
+import sys, heapq
 input = sys.stdin.readline
-
 
 def findMin():
     result = []
     for idx, val in product.items():
         if cost[start][val[1]] <= val[0]:
-            result.append([abs(val[0] - cost[start][val[1]]), idx])
+            result.append([val[0] - cost[start][val[1]], idx])
 
     if not result:
         return -1
@@ -17,25 +16,34 @@ def findMin():
         return result[0][1]
 
 
+def dijkstra():
+    q, dist = [], [1e9] * n
+    dist[start] = 0
+    heapq.heappush(q, (0, start))
+    while q:
+        time, cur = heapq.heappop(q)
+        if dist[cur] < time:
+            continue
+
+        for next in graph[cur]:
+            if next[1] + time < dist[next[0]]:
+                dist[next[0]] = next[1] + time
+                heapq.heappush(q, (dist[next[0]], next[0]))
+    cost[start] = dist
+
 if __name__ == "__main__":
     q = int(input())
     tmp = list(map(int, input().split()))
     n, m = tmp[1], tmp[2]
     product = defaultdict(list)
+    graph = [[] for _ in range(n)]
     cost = [[1e9] * n for _ in range(n)]
     start = 0
 
     for i in range(1, m + 1):
-        cost[tmp[i * 3]][tmp[i * 3 + 1]] = min(cost[tmp[i * 3]][tmp[i * 3 + 1]], tmp[i * 3 + 2])
-        cost[tmp[i * 3 + 1]][tmp[i * 3]] = min(cost[tmp[i * 3 + 1]][tmp[i * 3]], tmp[i * 3 + 2])
-
-    for i in range(n):
-        cost[i][i] = 0
-
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
+        if tmp[i * 3] != tmp[i * 3 + 1]:
+            graph[tmp[i * 3]].append((tmp[i * 3 + 1], tmp[i * 3 + 2]))
+            graph[tmp[i * 3 + 1]].append((tmp[i * 3], tmp[i * 3 + 2]))
 
     for i in range(q - 1):
         num, *tmp = list(map(int, input().split()))
@@ -47,6 +55,7 @@ if __name__ == "__main__":
             else:
                 product.pop(tmp[0])
         elif num == 400:
+            dijkstra()
             print(findMin())
         elif num == 500:
             start = tmp[0]
