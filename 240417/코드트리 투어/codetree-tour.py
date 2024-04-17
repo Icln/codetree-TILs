e1b97033ee9f5a1456e05d64fd91360f70290b2a
임강호 <55732952+Icln@users.noreply.github.com>
2024-04-17 19:49:50 +0900

    Title: 코드트리 투어, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240417/코드트리 투어/codetree-tour.py
+++ 240417/코드트리 투어/codetree-tour.py
@@ -2,18 +2,10 @@ from collections import defaultdict
 import sys, heapq
 input = sys.stdin.readline
 
-def findMin():
-    result = []
-    for idx, val in product.items():
-        if cost[start][val[1]] <= val[0]:
-            result.append([val[0] - cost[start][val[1]], idx])
-
-    if not result:
-        return -1
-    else:
-        result.sort(key=lambda x: (-x[0], x[1]))
-        product.pop(result[0][1])
-        return result[0][1]
+def renew():
+    for key, val in product.items():
+        product[key] = [(val[1] - cost[val[2]]) * -1, val[1], val[2]]
+        heapq.heappush(result, [(val[1] - cost[val[2]]) * -1, key])
 
 
 def dijkstra():
@@ -24,12 +16,13 @@ def dijkstra():
         time, cur = heapq.heappop(q)
         if dist[cur] < time:
             continue
-
         for next in graph[cur]:
             if next[1] + time < dist[next[0]]:
                 dist[next[0]] = next[1] + time
                 heapq.heappush(q, (dist[next[0]], next[0]))
-    cost[start] = dist
+
+    return dist
+
 
 if __name__ == "__main__":
     q = int(input())
@@ -37,7 +30,6 @@ if __name__ == "__main__":
     n, m = tmp[1], tmp[2]
     product = defaultdict(list)
     graph = [[] for _ in range(n)]
-    cost = [[1e9] * n for _ in range(n)]
     start = 0
 
     for i in range(1, m + 1):
@@ -45,18 +37,34 @@ if __name__ == "__main__":
             graph[tmp[i * 3]].append((tmp[i * 3 + 1], tmp[i * 3 + 2]))
             graph[tmp[i * 3 + 1]].append((tmp[i * 3], tmp[i * 3 + 2]))
 
-    dijkstra()
+    cost = dijkstra()
+    result = []
     for i in range(q - 1):
         num, *tmp = list(map(int, input().split()))
         if num == 200:
-            product[tmp[0]] = [tmp[1], tmp[2]]
+            product[tmp[0]] = [(tmp[1] - cost[tmp[2]]) * -1, tmp[1], tmp[2]]
+            heapq.heappush(result, [(tmp[1] - cost[tmp[2]]) * -1, tmp[0]])
+
         elif num == 300:
             if product[tmp[0]] != list():
+                result.remove([product[tmp[0]][0], tmp[0]])
                 product.pop(tmp[0])
             else:
                 product.pop(tmp[0])
+
         elif num == 400:
-            print(findMin())
+            if not result:
+                print(-1)
+            else:
+                if result[0][0] > 0:
+                    print(-1)
+                else:
+                    node = heapq.heappop(result)
+                    print(node[1])
+                    product.pop(node[1])
+
         elif num == 500:
             start = tmp[0]
-            dijkstra()
+            cost = dijkstra()
+            result = []
+            renew()
